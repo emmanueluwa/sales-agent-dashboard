47202ca0c41bc7ea8927ef4734c9010cea325ec6
emmanueluwa <oemmanuel.uwadiae@gmail.com>
2026-06-04 20:47:26 +0100

    add lib re-exports

---
+++ lib/index.ts
@@ -0,0 +1,11 @@
+/**
+   public API for the lib module.
+   import everything from here rather than from individual modules.
+ */
+
+export * from "./api/client";
+export * from "./api/stats";
+export * from "./api/leads";
+export * from "./api/videos";
+export * from "./api/outreach";
+export * from "./api/calls";
